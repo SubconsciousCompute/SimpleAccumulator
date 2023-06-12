cd0054c7077637b5497e549b6327ddafccb8691b
Purnata Ghosal <purnatag@gmail.com>
2023-06-12 11:03:54 +0530

    error_mean does not write temp data anymore

--- examples/error_mean.rs
+++ examples/error_mean.rs
@@ -1,8 +1,4 @@
 use rand::Rng;
-use std::fs::File;
-use std::io::prelude::*;
-//use std::io::{self, ErrorKind};
-use std::path::Path;
 
 use simple_accumulator::SimpleAccumulator;
 
@@ -17,15 +13,7 @@ fn main() {
     let base:i32 = 10;
     let multiplier = base.pow(5) as f64;
 
-    //Write error into a text file
-    let path = Path::new("error_in_mean.txt");
-    let display = path.display();
-    // Open a file in write-only mode, returns `io::Result<File>`
-    let mut file = match File::create(&path) {
-        Err(why) => panic!("couldn't create {}: {}", display, why),
-        Ok(file) => file,
-    };
-
+    println!("Waiting to plot the error data...");
     for _i in 0..1000 {
         for _j in 0..1000{
             let data = rand::thread_rng().gen::<f64>();
@@ -36,14 +24,8 @@ fn main() {
         let error_diff = (offline_mean - mean)/acc.len as f64; 
         error_mean.push(error_diff*multiplier);
         len_per_error_mean.push(acc.len as f64);
-
-        // Write the error to `file`, returns `io::Result<()>`
-        match file.write_all(format!("Error for mean: {}  Number of data points: {} \n", error_diff*multiplier, acc.len).as_bytes()) {
-            Err(why) => panic!("couldn't write to {}: {}", display, why),
-            Ok(_) => println!("\n successfully wrote to {}", display),
-        }
     }
-
+    
     // Plot the error data
     let trace = Scatter::new(len_per_error_mean, error_mean)
         .name("trace")
