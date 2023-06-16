6c37bfd712bdb5b94d3388ddbef1817e721edcda
Dilawar Singh <dilawar@subcom.tech>
2023-06-16 11:45:03 +0530

    chore: deps upgrade

--- examples/error_mean.rs
+++ examples/error_mean.rs
@@ -1,31 +1,28 @@
-use rand::Rng;
-
-use simple_accumulator::SimpleAccumulator;
-
 use plotly::common::Mode;
-//use plotly::layout::{Axis, BarMode, Layout, Legend, TicksDirection};
 use plotly::{Plot, Scatter};
+use rand::Rng;
+use simple_accumulator::SimpleAccumulator;
 
 fn main() {
     let mut acc = SimpleAccumulator::new::<f64>(&[], true);
-    let mut error_mean:Vec<f64> = Vec::new();
-    let mut len_per_error_mean:Vec<f64> = Vec::new();
-    let base:i32 = 10;
+    let mut error_mean: Vec<f64> = Vec::new();
+    let mut len_per_error_mean: Vec<f64> = Vec::new();
+    let base: i32 = 10;
     let multiplier = base.pow(5) as f64;
 
     println!("Waiting to plot the error data...");
     for _i in 0..1000 {
-        for _j in 0..1000{
+        for _j in 0..1000 {
             let data = rand::thread_rng().gen::<f64>();
             acc.push(data);
         }
         let mean = acc.mean;
         let offline_mean = acc.calculate_mean();
-        let error_diff = (offline_mean - mean)/acc.len as f64; 
-        error_mean.push(error_diff*multiplier);
+        let error_diff = (offline_mean - mean) / acc.len as f64;
+        error_mean.push(error_diff * multiplier);
         len_per_error_mean.push(acc.len as f64);
     }
-    
+
     // Plot the error data
     let trace = Scatter::new(len_per_error_mean, error_mean)
         .name("trace")
